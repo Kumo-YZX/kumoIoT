81d0794a3192668821cfc283145056f2e8bfbb75
Kumo Lam <16816538+Kumo-YZX@users.noreply.github.com>
2018-04-28 03:27:14 +0800

    fix bugs in server

--- server.py
+++ server.py
@@ -1,12 +1,36 @@
 import tornado.ioloop
 import tornado.web
+import tornado.template
+import jinja2
+import threading
+
+class myTemplate(object):
+    def __init__(self, template_instance):
+        self.template_instance =template_instance
+
+    def generate(self, **kwargs):
+        return self.template_instance.render(**kwargs)
+
+class Jinja2Loader(tornado.template.BaseLoader):
+    def __init__(self, **kwargs):
+        self.jinja_environment =jinja2.Environment(loader=jinja2.FileSystemLoader('pages/'), **kwargs)
+        self.templates ={}
+        self.lock =threading.RLock()
+
+    def resolve_path(self, name, parent_path =None):
+        return name
+
+    def _create_template(self, name):
+        template_instance =myTemplate(self.jinja_environment.get_template(name))
+        return template_instance
 
 class mainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("page/homepage.html", mytitle="Welcome to KumoIoT API", parameter=["function1","function2","function3"])
+        self.render("homepage.html", mytitle="Welcome to KumoIoT API", parameter=["function1","function2","function3"])
 
 def startApp():
-    return tornado.web.Application([
+    return tornado.web.Application(template_loader=Jinja2Loader(),
+     handlers=[
         (r"/", mainHandler)
     ])
 
